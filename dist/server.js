13b738455d75981d8bc5f593a97edb795f39529f
koya <koya9431ptr@gmail.com>
2025-03-10 19:29:05 +0000

    done

--- dist/server.js
+++ dist/server.js
@@ -1,2 +1,27 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
 // This is your server file :)
+const express_1 = __importDefault(require("express"));
+const path_1 = __importDefault(require("path"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const app = (0, express_1.default)();
+// Set EJS as view engine
+app.set('view engine', 'ejs');
+app.set('views', path_1.default.join(__dirname, '../src/views'));
+// Serve static files from the public directory (located in dist)
+app.use(express_1.default.static(path_1.default.join(__dirname, '../dist/public')));
+// Import and mount routes
+const home_1 = __importDefault(require("./routes/home"));
+const about_1 = __importDefault(require("./routes/about"));
+const contact_1 = __importDefault(require("./routes/contact"));
+app.use('/', home_1.default);
+app.use('/about', about_1.default);
+app.use('/contact', contact_1.default);
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,28 @@
 // This is your server file :)
+import express from 'express'
+import path from 'path'
+import dotenv from 'dotenv'
+
+dotenv.config()
+const app = express()
+
+// Set EJS as view engine
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, '../src/views'))
+
+// Serve static files from the public directory (located in dist)
+app.use(express.static(path.join(__dirname, '../dist/public')))
+
+// Import and mount routes
+import homeRouter from './routes/home'
+import aboutRouter from './routes/about'
+import contactRouter from './routes/contact'
+
+app.use('/', homeRouter)
+app.use('/about', aboutRouter)
+app.use('/contact', contactRouter)
+
+const PORT = 3000
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`)
+})
